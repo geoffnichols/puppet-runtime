e5643d1bdac8d1805742f59f6e2b22184f66154b
Geoff Nichols <geoff.nichols@puppet.com>
2018-07-04 21:32:02 -0700

    (maint) Use yum to install build dependencies on AIX
    As a POC, update the AIX platform configs to bootstrap and use yum
    to install build dependencies.

--- configs/platforms/aix-7.1-ppc.rb
+++ configs/platforms/aix-7.1-ppc.rb
@@ -6,15 +6,16 @@ platform "aix-7.1-ppc" do |plat|
   plat.rpmbuild "/usr/bin/rpm"
   plat.patch "/opt/freeware/bin/patch"
 
-  # Basic vanagon operations require mktemp, rsync, coreutils, tar and sed so leave this in there
-  plat.provision_with "rpm -Uvh --replacepkgs http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/mktemp-1.7-1.aix5.1.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/rsync-3.0.6-1.aix5.3.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/coreutils-5.2.1-2.aix5.1.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/sed-4.1.1-1.aix5.1.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/make-3.80-1.aix5.1.ppc.rpm http://osmirror.delivery.puppetlabs.net/AIX_MIRROR/tar-1.22-1.aix6.1.ppc.rpm"
-
-  # We use --force with rpm because the pl-gettext and pl-autoconf
-  # packages conflict with a charset.alias file.
-  #
-  # Until we get those packages to not conflict (or we remove the need
-  # for pl-autoconf) we'll need to force the installation
-  #                                         Sean P. McD.
-  plat.install_build_dependencies_with "rpm -Uvh --replacepkgs --force "
+  plat.provision_with "curl -O http://pl-build-tools.delivery.puppetlabs.net/aix/yum_bootstrap/rpm.rte && installp -acXYgd . rpm.rte all"
+  plat.provision_with "curl http://pl-build-tools.delivery.puppetlabs.net/aix/yum_bootstrap/openssl-1.0.2.1500.tar | tar xvf - && cd openssl-1.0.2.1500 && installp -acXYgd . openssl.base all"
+  plat.provision_with "rpm --rebuilddb && updtvpkg"
+  plat.provision_with "mkdir -p /tmp/yum_bundle && cd /tmp/yum_bundle/ && curl -O http://pl-build-tools.delivery.puppetlabs.net/aix/yum_bootstrap/yum_bundle.tar && tar xvf yum_bundle.tar && rpm -Uvh /tmp/yum_bundle/*.rpm ||:"
+  # TODO: Uncomment the following lines after updating artifactory to mirror IBM's yum repositories:
+  # plat.provision_with "/usr/bin/sed 's/enabled=1/enabled=0/g' /opt/freeware/etc/yum/yum.conf > tmp.$$ && mv tmp.$$ /opt/freeware/etc/yum/yum.conf"
+  # plat.provision_with "echo '[AIX_Toolbox_mirror]\nname=AIX Toolbox local mirror\nbaseurl=https://artifactory.delivery.puppetlabs.net/artifactory/rpm__remote_aix_linux_toolbox/ppc/\ngpgcheck=0' > /opt/freeware/etc/yum/repos.d/toolbox-generic-mirror.repo"
+  # plat.provision_with "echo '[AIX_Toolbox_noarch_mirror]\nname=AIX Toolbox noarch repository\nbaseurl=https://artifactory.delivery.puppetlabs.net/artifactory/rpm__remote_aix_linux_toolbox/noarch/\ngpgcheck=0' > /opt/freeware/etc/yum/repos.d/toolbox-noarch-mirror.repo"
+  # plat.provision_with "echo '[AIX_Toolbox_71_mirror]\nname=AIX 61 specific repository\nbaseurl=https://artifactory.delivery.puppetlabs.net/artifactory/rpm__remote_aix_linux_toolbox/ppc-7.1/\ngpgcheck=0' > /opt/freeware/etc/yum/repos.d/toolbox-71-mirror.repo"
+  plat.provision_with "yum install -y rsync coreutils sed make tar pkg-config zlib zlib-devel gawk autoconf gcc glib2"
+  plat.install_build_dependencies_with "yum install -y "
   plat.vmpooler_template "aix-7.1-power"
 end
